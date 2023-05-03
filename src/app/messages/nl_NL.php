45c1b1f5dbeb52d7b7573b049769a20d7a98dd48
Anuj Maurya <anujmaurya@cedcoss.com>
2023-05-03 12:19:05 +0530

    initial commit

---
+++ src/app/messages/nl_NL.php
@@ -0,0 +1,17 @@
+<?php
+return $messages = [
+    'admin' => 'beheerder',
+    'signup' => 'aanmelden',
+    'Name' => 'naam',
+    'Password' => 'Wachtwoord',
+    'Choose the role' => 'Kies een rol',
+    'Sign Up' => 'Aanmelden',
+    'Choose' => 'Kiezen',
+    'Language' => 'Taal',
+    'email' => 'e-mailen',
+    'User' => 'gebruiker',
+    'Access denied' => 'Toegang geweigerd',
+    'denied' => 'geweigerd',
+    'Access Granted' => 'Toegang toegekend',
+    'Product Table' => 'Producttabel',
+];
